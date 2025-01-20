c0883d039e3b96a103ec52716c8e8ca43dd3b1af
Zheng Te <1221537+tezheng@users.noreply.github.com>
2025-01-20 08:21:01 +0800

    WIP

--- models/llm/llmlingua/data.py
+++ models/llm/llmlingua/data.py
@@ -1,20 +1,51 @@
 
-
 from torch.utils.data import DataLoader, Dataset
 
+from transformers import (AutoTokenizer, AutoModel)
+
+from olive.data.component.dataset import BaseDataset
 from olive.data.registry import Registry
 
 
-@Registry.register_dataloader()
-def llmlingua_calibration_reader(dataset, batch_size, data_dir, **kwargs):
-    class MobileNetDataset(Dataset):
-        def __init__(self, data_dir):
-        self.data = np.load(data_dir)
+@Registry.register_pre_process()
+def llm_pre_process(dataset, model_name, input_cols, label_col="label", **kwargs):
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModel.from_pretrained(model_name)
+    model.eval()
 
-        def __len__(self):
-        return len(self.data)
+    target = model.encoder
 
-        def __getitem__(self, idx):
-        return self.data[idx
-  dataset= MobileNetDataset(data_dir)
-  return DataLoader(dataset, batch_size=batch_size, shuffle=False)
+    from inspect import signature
+    params = [n for (n, _) in signature(target.forward).parameters.items()]
+
+    inputs = {}
+
+    def capture_inputs(_, args, kwargs):
+        for i, arg in enumerate(args):
+            if isinstance(arg, torch.Tensor):
+                inputs[params[i]] = arg[0,].clone()
+        for key, value in kwargs.items():
+            if isinstance(value, torch.Tensor):
+                inputs[key] = value[0,].clone()
+        return None
+
+    def inference(examples):
+        inputs.clear()
+        encoded_input = tokenizer(
+            *[examples[input_col] for input_col in input_cols],
+            padding='max_length',
+            max_length=512,
+            truncation=True,
+            return_tensors='pt'
+        )
+        with torch.inference_mode():
+            model(**encoded_input)
+        inputs[label_col] = examples[label_col]
+        return inputs.copy()
+
+    target.register_forward_pre_hook(capture_inputs, with_kwargs=True)
+
+    return BaseDataset(
+        [inference(i) for i in dataset.select(range(4))],
+        label_col
+    )
